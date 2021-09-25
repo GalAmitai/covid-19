37f25af6fee5005b0d5f50fa9c8746027b2a6d17
GalAmitaiGP <gal.amitai@getpackage.com>
2021-09-25 15:14:39 +0300

    ghpages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  rapid_host: "https://covid-19-data.p.rapidapi.com",
+  header_x_rapid_host: "covid-19-data.p.rapidapi.com",
+  header_x_rapid_key: "c307fe4f01msheec638cb207b201p14a613jsnb0e9dec31a04"
 };
